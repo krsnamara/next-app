31951a69a6de3c5f210df99f0cb672bfd81d98a4
krsnamara <martinj.fitzpatrick@gmail.com>
2023-12-04 22:35:34 -0500

    initial structure"

---
+++ app/users/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Link from 'next/link';
+
+const UsersPage = () => {
+  return (
+    <>
+      <Link href="/">Home</Link>
+      <div>UsersPage</div>
+    </>
+  );
+};
+
+export default UsersPage;
