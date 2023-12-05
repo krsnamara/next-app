5d0ca8c6502dc83a14da07b8e159298917599e3c
krsnamara <martinj.fitzpatrick@gmail.com>
2023-12-04 22:43:16 -0500

    initial commit

--- app/components/AddToCart.tsx
+++ app/components/AddToCart.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const AddToCart = () => {
   return (
     <div>
-      <button onClick={() => console.log('Click')}>Add to Cart</button>
+      <button onClick={() => console.log('Click')}>Add to Cart yes</button>
     </div>
   );
 };
